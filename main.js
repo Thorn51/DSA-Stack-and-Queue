5e2448bf1266fecb25b3768b7a42938f113b59bd
Matt Thornberry <mtberry16@hotmail.com>
2020-04-17 19:59:15 -0600

    complete stack drills 1-3

--- main.js
+++ main.js
@@ -1,22 +1,90 @@
-const Stack = require("./stack");
-const Queue = require("./queue");
+import Stack from "./stack.js";
+import Queue from "./queue.js";
 
-// const testStack = new Stack();
+const starTrek = new Stack();
+starTrek.push("Kirk");
+starTrek.push("Spock");
+starTrek.push("McCoy");
+starTrek.push("Scotty");
 
-// testStack.push(10);
-// testStack.push(20);
-// testStack.push(30);
-// testStack.push(40);
-// testStack.pop();
-// testStack.peek();
+const emptyStack = new Stack();
 
-// console.log(testStack);
+const palindromes = new Stack();
+palindromes.push("Dad");
+palindromes.push("Mom");
+palindromes.push("A man, a plan, a canal: Panama");
+palindromes.push("1,001");
+palindromes.push("Taco");
 
-let testQueue = new Queue();
+function stackClass() {
+  console.log("1. Create a linked stack class");
+  console.log(starTrek);
+}
 
-testQueue.enqueue(10);
-testQueue.enqueue(20);
-testQueue.enqueue(30);
-testQueue.dequeue();
+function peek(stack) {
+  console.log("2. Useful methods for a stack");
+  console.log("2a. peek()");
+  if (stack.top === null) {
+    return null;
+  }
+  let topStack = stack.top;
+  //   console.log(topStack);
+  return topStack;
+}
 
-console.log(testQueue);
+function isEmpty(stack) {
+  console.log("2b. isEmpty()");
+  if (stack.top === null) {
+    return true;
+  }
+  return false;
+}
+
+function display(stack) {
+  console.log("2c. display()");
+  if (stack.top === null) {
+    return "Stack is empty";
+  }
+  let node = stack.top;
+  let nodeValues = "";
+  while (node) {
+    nodeValues += node.value + " ";
+    node = node.next;
+  }
+  return nodeValues;
+}
+
+function isPalindrome(string) {
+  console.log("3. Check for palindromes using a stack");
+  string = string.toLowerCase().replace(/\W/g, "");
+  let palindromeCharacters = new Stack();
+  let reverseString = "";
+  for (let i = 0; i < string.length; i++) {
+    palindromeCharacters.push(string[i]);
+  }
+  while (palindromeCharacters.top !== null) {
+    reverseString += palindromeCharacters.top.value;
+    palindromeCharacters.pop();
+  }
+  if (string !== reverseString) {
+    console.log(
+      "Original string ->",
+      string,
+      "Reverse string->",
+      reverseString
+    );
+    return false;
+  }
+  console.log("Original string ->", string, "Reverse string->", reverseString);
+  return true;
+}
+
+stackClass();
+console.log("Peek starTrek ->", peek(starTrek));
+console.log("isEmpty starTrek ->", isEmpty(starTrek));
+console.log("isEmpty emptyStack ->", isEmpty(emptyStack));
+console.log("With function on starTrek ->", display(starTrek));
+console.log("With method on emptyStack ->", emptyStack.display());
+console.log(isPalindrome("dad"));
+console.log(isPalindrome("taco"));
+console.log(isPalindrome("A man, a plan, a canal: Panama"));
