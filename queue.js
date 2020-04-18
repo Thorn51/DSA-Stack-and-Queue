f02b435e31ddc3b93abb41e095c70d8a5ddfe954
Matt Thornberry <mtberry16@hotmail.com>
2020-04-17 19:59:45 -0600

    Create queue class

--- queue.js
+++ queue.js
@@ -1,7 +1,7 @@
 // Create node class
 class _Node {
-  constructor(data) {
-    this.data = data;
+  constructor(value) {
+    this.value = value;
     this.next = null;
   }
 }
@@ -9,41 +9,58 @@ class _Node {
 //Create queue class
 class Queue {
   constructor() {
+    // Track the first item in the queue
     this.first = null;
+    // Track the last item in the queue
     this.last = null;
+    // Track the size of the queue
     this.size = 0;
   }
 
-  //Add items to the queue
-  enqueue(data) {
-    const node = new _Node(data);
+  // Add nodes to the queue
+  enqueue(value) {
+    // Create a new node to store the value
+    let node = new _Node(value);
 
+    // If the node is the first to be added
     if (this.first === null) {
       this.first = node;
+      this.size++;
     }
 
+    // Shift existing node in last to the next position in queue
     if (this.last) {
       this.last.next = node;
     }
+
+    // Make the new node last
     this.last = node;
     this.size++;
   }
 
-  //Remove items from the queue
+  // Remove an item from the queue
   dequeue() {
+    // Check if queue is empty
     if (this.first === null) {
-      console.log("Queue is empty");
+      return null;
     }
+
+    // Get the first node in the list
     let node = this.first;
+
+    // Shift the next node in the queue to first
     this.first = this.first.next;
 
-    //If last item in queue
+    // If the node is in the last position of the queue
     if (node === this.last) {
       this.last = null;
     }
-    this.size--;
     return node.value;
   }
 }
 
-module.exports = Queue;
+//Add items to the queue
+
+//Remove items from the queue
+
+export default Queue;
